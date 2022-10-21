60554f9aae80e3e35f5f71e94c06e3736cbb9b38
jvidalv <josepvidalvidal@gmail.com>
2022-10-21 19:13:56 +0200

    Removed page api route

--- lib/request.ts
+++ lib/request.ts
@@ -1,8 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-const domain =
-  process.env.NODE_ENV === "production"
-    ? "https://grafbase-nextjs-hackernews-with-clerk.grafbase-vercel.dev/api/graphql"
-    : "http://localhost:3000/graphql";
-
-export const graphQlRequestClient = new GraphQLClient(domain);
+export const graphQlRequestClient = new GraphQLClient(
+  process.env.NEXT_PUBLIC_GRAFBASE_API_URL!
+);
