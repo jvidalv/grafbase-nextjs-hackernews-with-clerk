64923d64c4b13bed265cb3c43b6a33f5a480bccc
jvidalv <josepvidalvidal@gmail.com>
2023-01-09 14:08:49 +0100

    Switched to npm

--- lib/request.ts
+++ lib/request.ts
@@ -1,11 +1,11 @@
-import { GraphQLClient } from 'graphql-request'
+import { GraphQLClient } from "graphql-request";
 
 export const graphQlRequestClient = new GraphQLClient(
   process.env.NEXT_PUBLIC_GRAFBASE_API_URL!,
   {
     headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': process.env.GRAFBASE_API_KEY!
-    }
+      "Content-Type": "application/json",
+      "x-api-key": process.env.GRAFBASE_API_KEY!,
+    },
   }
-)
+);
